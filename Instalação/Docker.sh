9b5a7665093227e225f571e5687dd303bc8ed284
docksec <152213627+docksec@users.noreply.github.com>
2024-02-12 23:59:14 -0300

    Update Docker.sh

--- Instalação/Docker.sh
+++ Instalação/Docker.sh
@@ -4,10 +4,10 @@
 sudo apt-get update
 
 # Passo 2: Instalar as ferramentas necessárias
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
 # Passo 3: Criar um diretório para armazenar a chave GPG
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 
 # Passo 4: Baixar a chave GPG oficial do Docker e salvar no diretório '/etc/apt/keyrings/docker.gpg'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -16,13 +16,18 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Passo 6: Adicionar o repositório do Docker às fontes do APT
-echo "deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ARCH=$(dpkg --print-architecture)
+CODENAME=$(lsb_release -cs)
+echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Passo 7: Atualizar a lista de pacotes para incluir o repositório do Docker
 sudo apt-get update
 
 # Passo 8: Instalação do Docker e Docker Compose
-sudo apt install docker.io docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
-# Passo 9: Verificar se o Docker foi instalado corretamente
+# Passo 9: Adicionar o usuário ao grupo Docker
+sudo usermod -aG docker $(whoami)
+
+# Passo 10: Verificar se o Docker foi instalado corretamente
 docker --version
