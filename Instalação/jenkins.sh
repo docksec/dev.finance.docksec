90b2e1f7ac35e19f7a6b40af53a20362c4953738
docksec <152213627+docksec@users.noreply.github.com>
2024-02-25 00:11:49 -0300

    Update jenkins.sh

--- Instalação/jenkins.sh
+++ Instalação/jenkins.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 sudo apt update -y
-#Instalação Java Temurin 
-wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz
-tar -xvf OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz
-sudo mkdir -p /usr/lib/jvm
-sudo mv jdk-21.0.2 /usr/lib/jvm/
-export JAVA_HOME=/usr/lib/jvm/jdk-21.0.2
-export PATH=$JAVA_HOME/bin:$PATH
-source ~/.bashrc
+#sudo apt upgrade -y
+sudo apt install openjdk-17-jdk
 java -version
-#Instalação do Jenkins
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
                   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
